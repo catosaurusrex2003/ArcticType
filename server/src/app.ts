ecb718d1c314f364f43e6316e6500c3808404150
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2023-06-17 00:45:28 +0530

    trailing slash removed

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin:["http://localhost:3000","https://fuschia-racer.vercel.app/"]
+  origin:["http://localhost:3000","https://fuschia-racer.vercel.app"]
 }))
 // app.use(errorHandler);
 
