a0968f9f6853b1c92be338c78d286b9acff1c408
catosaurusrex2003 <your-email@example.com>
2023-07-09 14:54:30 +0530

    Deploy project

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# Set your Git credentials
+GIT_USERNAME="catosaurusrex2003"
+GIT_PASSWORD="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" # Use your personal access token here
+
 # Build the client
 cd server
 npm run build
 
 # Go back to the root directory
 cd ..
+
+# Configure Git with your credentials
+git config --global credential.helper "store --file ~/.git-credentials"
+git config --global user.name "$GIT_USERNAME"
+git config --global user.email "your-email@example.com" # Use your email associated with Git
+
+# Store the Git credentials
+echo "https://$GIT_USERNAME:$GIT_PASSWORD@github.com" > ~/.git-credentials
 
 # Commit and push to GitHub
 git add .
