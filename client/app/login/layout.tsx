4b483c496a406dc293519cd2625d5701a4fdde76
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2023-09-07 20:29:57 +0530

    Deploy project 08:29PM September 07

--- client/app/login/layout.tsx
+++ client/app/login/layout.tsx
@@ -4,10 +4,10 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 // PROTECTED ROUTE
-function layout({ children }: { children: React.ReactNode }) {
+function Layout({ children }: { children: React.ReactNode }) {
   const auth = useGeneralStore((store) => store.auth);
   if (auth) redirect("/profile");
   else return <div>{children}</div>;
 }
 
-export default layout;
+export default Layout;
